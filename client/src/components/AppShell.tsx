c284d4713bf34aa35f75bce8c7566406765dd174
Saultr21 <liagsad21@gmail.com>
2026-06-11 21:00:01 +0100

    feat(client): atajos globales de teclado con desplazamiento accesible de overlays

--- client/src/components/AppShell.tsx
+++ client/src/components/AppShell.tsx
@@ -1,11 +1,23 @@
+import { useEffect } from "react";
 import { ImagePanel } from "../features/image/ImagePanel";
 import { MediaPanel } from "../features/media/MediaPanel";
-import { PlaybackProvider, PreviewArea } from "../features/preview/PreviewArea";
+import { PlaybackProvider, PreviewArea, usePlayback } from "../features/preview/PreviewArea";
 import { PropertiesPanel } from "../features/properties/PropertiesPanel";
 import { Timeline } from "../features/timeline/Timeline";
 import { useUiStore } from "../stores/uiStore";
+import { handleShortcut } from "../lib/shortcuts";
 import { TopBar } from "./TopBar";
 import { ToolRail } from "./ToolRail";
+
+function GlobalShortcuts() {
+  const { seek, togglePlay } = usePlayback();
+  useEffect(() => {
+    const onKey = (e: KeyboardEvent) => handleShortcut(e, { seek, togglePlay });
+    window.addEventListener("keydown", onKey);
+    return () => window.removeEventListener("keydown", onKey);
+  }, [seek, togglePlay]);
+  return null;
+}
 
 export function AppShell() {
   const activeTool = useUiStore((s) => s.activeTool);
@@ -13,6 +25,7 @@ export function AppShell() {
     <div className="h-screen flex flex-col">
       <TopBar />
       <PlaybackProvider>
+        <GlobalShortcuts />
         <div className="flex flex-1 min-h-0">
           <ToolRail />
           <main className="flex flex-1 min-w-0">
